eac65370fbd8beccdaf00f9eb9e13c7c98d198a7
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-01-07 18:38:23 +0100

    GameObject.java interface

--- src/main/java/it/unibo/model/api/GameObject.java
+++ src/main/java/it/unibo/model/api/GameObject.java
@@ -1,6 +1,16 @@
 package it.unibo.model.api;
 
-public interface GameObject {
-    
+import java.awt.Image;
+import java.awt.geom.Point2D;
+import java.awt.geom.Dimension2D;
 
+public interface GameObject {
+
+    Point2D.Double getPosition();
+
+    void setPosition(Point2D.Double position);
+
+    Image getImage();
+
+    Dimension2D getDimension();
 }
