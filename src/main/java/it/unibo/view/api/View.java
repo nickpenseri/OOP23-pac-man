0404bd310984c39b1e1f6e074a119b13ebe460d5
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-01-07 17:45:07 +0100

    View interface initialization

--- src/main/java/it/unibo/view/api/View.java
+++ src/main/java/it/unibo/view/api/View.java
@@ -1,5 +1,21 @@
 package it.unibo.view.api;
 
+import java.util.List;
+
+/** 
+ * The View interface is used to obtain user commands and model object logic.
+ * Once obtained, update the graphics and pass the command to the controller on request.
+*/
 public interface View {
 
+    /**
+     * Update the game graphics
+     */
+    public void updateView();
+
+    /**
+     * 
+     * @return return a list commands user
+     */
+    public List<Command> getListCommands(); 
 }
