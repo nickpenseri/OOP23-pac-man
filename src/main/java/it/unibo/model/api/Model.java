4b2e669ec65ce4c3fc8a9066ebe89e570b8ee0ca
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-01-07 17:29:04 +0100

    Initialized Model.java

--- src/main/java/it/unibo/model/api/Model.java
+++ src/main/java/it/unibo/model/api/Model.java
@@ -1,5 +1,22 @@
 package it.unibo.model.api;
+import java.util.List;
 
+/** Is the container and executor of the GameObjects */
 public interface Model {
 
+    /** Get the list of objects in the model 
+     * @return the list of objects in the model
+    */
+    public List<GameObject> getObjects();
+
+    /** Get the command imput passed and do it 
+     * @param command the command we need do
+    */
+    public void processInput(Command command);
+
+    /** Update the state of the objects in the model */
+    public void updateState();
+
+    /** Initialize the game */
+    public void initGame();
 }
