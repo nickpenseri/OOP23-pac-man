3bc7a0871b5b62e807df1f0a96cfcb4b5742f031
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2024-01-10 16:16:43 +0100

    Fixed javadoc comments in GameObject for static analysis

--- src/main/java/it/unibo/model/api/GameObject.java
+++ src/main/java/it/unibo/model/api/GameObject.java
@@ -4,31 +4,31 @@ import java.awt.Image;
 import java.awt.geom.Point2D;
 import java.awt.geom.Dimension2D;
 
-/** Basic entity of a game scene */
+/** Basic entity of a game scene. */
 public interface GameObject {
 
     /**
-     * Get the position of the object
-     * @return
+     * Get the position of the object.
+     * @return Point 2D of the position.
      */
 
     Point2D.Double getPosition();
 
     /**
-     * Set the position of the object in the scene
-     * @param position a 2D point
+     * Set the position of the object in the scene.
+     * @param position a 2D point.
      */
 
     void setPosition(Point2D.Double position);
 
     /**
-     * Get the image of the actual state of the game object
-     * @return Actual Image of the object
+     * Get the image of the actual state of the game object.
+     * @return Actual Image of the object.
      */
     Image getImage();
 
     /**
-     * get the actual dimension of the object in the scene
+     * get the actual dimension of the object in the scene.
      * @return actual dimension.
      */
 
