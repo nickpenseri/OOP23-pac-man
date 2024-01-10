7f9fe8752dbfeca30386f9c645eb31ac5340b602
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2024-01-10 17:34:52 +0100

    Created Character.java Interface

---
+++ src/main/java/it/unibo/model/api/Character.java
@@ -0,0 +1,30 @@
+package it.unibo.model.api;
+
+
+/** A GameObject that can be moved. */
+public interface Character extends GameObject{
+
+    /**
+     * Change the direction of the movement of the Character.
+     * @param direction The new direction of the Character.
+     */
+    void setDirection(Direction direction);
+    
+    /**
+     * update the state of the Character.
+     */
+    void updateState();
+
+    /**
+     * Increase the speed of the Character, unless it is already at maximum.
+     * @return true if the speed is changed 
+     */
+    boolean increaseSpeed();
+
+    /**
+     * Decrease the speed of the Character, unless it is already at minimum.
+     * @return true if the speed is changed 
+     */
+    boolean decreaseSpeed();
+
+}
