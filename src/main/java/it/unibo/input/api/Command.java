501e0d4e3694fe34f4d6d3ff0f834d2f9d27af40
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2024-01-10 16:53:17 +0100

    Created enum Command to manage command given from the user

--- src/main/java/it/unibo/input/api/Command.java
+++ src/main/java/it/unibo/input/api/Command.java
@@ -1,5 +1,23 @@
 package it.unibo.input.api;
-
-public interface Command {
-
+/**
+ * This enum is used to manage commands received from the view and passed to the model
+ * by the controller.
+ */
+public enum Command {
+    /**
+     * Command to change Pac-Man direction to up.
+     */
+    SET_DIR_UP,
+    /**
+     * Command to change Pac-Man direction to right.
+     */
+    SET_DIR_RIGHT,
+    /**
+     * Command to change Pac-Man direction to down.
+     */
+    SET_DIR_DOWN,
+    /**
+     * Command to change Pac-Man direction to left.
+     */
+    SET_DIR_LEFT;
 }
