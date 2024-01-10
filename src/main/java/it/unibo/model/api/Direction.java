8cee15dd0f019497943b258bda8ac3551d0de322
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2024-01-10 17:40:16 +0100

    Created enum direction to manage directions of the characters in the model

---
+++ src/main/java/it/unibo/model/api/Direction.java
@@ -0,0 +1,23 @@
+package it.unibo.model.api;
+
+/**
+ * This class is used to manage the direction of the characters in the model.
+ */
+public enum Direction {
+    /**
+     * Direction up.
+     */
+    UP,
+    /**
+     * Direction right.
+     */
+    RIGHT,
+    /**
+     * Direction down.
+     */
+    DOWN,
+    /**
+     * Direction left.
+     */
+    LEFT;
+}
