320e76edc9f7c426799a083dbb9a0673d1b1834b
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-01-10 16:35:51 +0100

    Modified Model.java

--- src/main/java/it/unibo/model/api/Model.java
+++ src/main/java/it/unibo/model/api/Model.java
@@ -3,22 +3,22 @@ package it.unibo.model.api;
 import java.util.List;
 import it.unibo.input.api.Command;
 
-/** Is the container and executor of the GameObjects */
+/** Is the container and executor of the GameObjects. */
 public interface Model {
 
-    /** Get the list of objects in the model 
+    /** Get the list of objects in the model. 
      * @return the list of objects in the model
     */
-    public List<GameObject> getObjects();
+    List<GameObject> getObjects();
 
-    /** Get the list of command imput passed and do it 
+    /** Get the list of command imput passed and do it. 
      * @param commands the command we need do
     */
-    public void processInput(List<Command> commands);
+    void processInput(List<Command> commands);
 
-    /** Update the state of the objects in the model */
-    public void updateState();
+    /** Update the state of the objects in the model. */
+    void updateState();
 
-    /** Initialize the game */
-    public void initGame();
+    /** Initialize the game. */
+    void initGame();
 }
