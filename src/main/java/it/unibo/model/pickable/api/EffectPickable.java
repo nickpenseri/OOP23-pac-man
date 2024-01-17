dd51ba1490489b2ef32611cb2a93178731f25fce
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-01-17 18:02:20 +0100

    Fix EffectPickable.java

--- src/main/java/it/unibo/model/pickable/api/EffectPickable.java
+++ src/main/java/it/unibo/model/pickable/api/EffectPickable.java
@@ -1,7 +1,11 @@
 package it.unibo.model.pickable.api;
 
+import it.unibo.model.pacman.api.PacMan;
+
 /** Un puckabble item with some effect to do. */
 public interface EffectPickable extends Pickable {
-    /** Do the effect of the pickable item. */
-    void doEffect();
+    /** Do the effect of the pickable item. 
+     * @param pacman the pacman that will be affected by the effect.
+    */
+    void doEffect(PacMan pacman);
 }
