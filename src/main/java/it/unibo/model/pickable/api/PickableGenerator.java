72b590777ff86dfa15e3ed7b53df7c6edef8ea67
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-01-19 18:02:22 +0100

    Fixed PickableGeneretor interface

--- src/main/java/it/unibo/model/pickable/api/PickableGenerator.java
+++ src/main/java/it/unibo/model/pickable/api/PickableGenerator.java
@@ -1,6 +1,7 @@
 package it.unibo.model.pickable.api;
 
 import java.awt.Point;
+import java.awt.geom.Dimension2D;
 import java.util.List;
 import java.util.Map;
 
@@ -13,7 +14,7 @@ public interface PickableGenerator {
      * 
      * @param pickableSpawnPoints is a list of Point where the pickable can spawn.
      */
-    void generateMap(List<Point> pickableSpawnPoints);
+    void generateMap(List<Point> pickableSpawnPoints, Dimension2D dimension);
 
     /**
      * Is for get the Map of Pickable.
@@ -29,4 +30,11 @@ public interface PickableGenerator {
      * @param pacman is the pacman that take the pickable.
      */
     void takePickable(Point point, PacMan pacman);
+
+    /**
+     * Is for know if the pickable is finished.
+     * 
+     * @return true if the pickable is finished.
+     */
+    boolean finishedPickable();
 }
