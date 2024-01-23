fab7c9b87725577cf7d7e5568fa9735c0c8781fa
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2024-01-23 19:02:50 +0100

    Created class ImageChooserImpl

---
+++ src/main/java/it/unibo/model/pacman/impl/ImageChooserImpl.java
@@ -0,0 +1,64 @@
+package it.unibo.model.pacman.impl;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import javax.imageio.ImageIO;
+import it.unibo.model.api.Direction;
+import it.unibo.model.pacman.api.ImageChooser;
+
+/**
+ * This class represents an implementation of ImageChooser, which can be updated
+ * and can give
+ * the actual image.
+ */
+public class ImageChooserImpl implements ImageChooser {
+    private static final String CLOSED_NAME = "";
+    private static final String UP_NAME = "";
+    private static final String RIGHT_NAME = "";
+    private static final String DOWN_NAME = "";
+    private static final String LEFT_NAME = "";
+    private static final URL CLOSED_URL = ClassLoader.getSystemResource(CLOSED_NAME);
+    private static final URL UP_URL = ClassLoader.getSystemResource(UP_NAME);
+    private static final URL RIGHT_URL = ClassLoader.getSystemResource(RIGHT_NAME);
+    private static final URL DOWN_URL = ClassLoader.getSystemResource(DOWN_NAME);
+    private static final URL LEFT_URL = ClassLoader.getSystemResource(LEFT_NAME);
+    private boolean closed;
+
+    /**
+     * Creates an object of this class which initial state is closed.
+     */
+    public ImageChooserImpl() {
+        this.closed = true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update() {
+        this.closed = !this.closed;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Image actualImage(final Optional<Direction> dir) {
+        Image actual = new BufferedImage(0, 0, 0);
+        try {
+            actual = ImageIO.read(dir.isEmpty() || this.closed ? CLOSED_URL : switch (dir.get()) {
+                case UP -> UP_URL;
+                case RIGHT -> RIGHT_URL;
+                case DOWN -> DOWN_URL;
+                case LEFT -> LEFT_URL;
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return actual;
+    }
+
+}
