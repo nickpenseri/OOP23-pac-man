5508e089b3ff1a40f7de18c70f0193fe0bd48ca0
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-01-24 19:01:14 +0100

    Created ControllerImpl.java class

---
+++ src/main/java/it/unibo/controller/impl/ControllerImpl.java
@@ -0,0 +1,39 @@
+package it.unibo.controller.impl;
+
+import java.util.Objects;
+import it.unibo.controller.api.Controller;
+import it.unibo.model.api.Model;
+import it.unibo.view.api.View;
+
+/** The class is the implementation of the Controller Interface. */
+public final class ControllerImpl implements Controller {
+
+    private final  Model model;
+    private final View view;
+
+    /**
+     * When Controller is created, it needs a model and a view to manage.
+     * @param model the model of the gameScene
+     * @param view the view of the gameScene
+     */
+    public ControllerImpl(final Model model, final View view) {
+        this.model = Objects.requireNonNull(model);
+        this.view = Objects.requireNonNull(view);
+    }
+
+    @Override
+    public void processInput() {
+        model.processInput(view.getListCommands());
+    }
+
+    @Override
+    public void updateState() {
+       model.updateState();
+    }
+
+    @Override
+    public void updateView() {
+        view.updateView(model.getObjects());
+    }
+
+}
