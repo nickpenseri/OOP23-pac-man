c05b85fa5a9c756b1e6285ecad8c3372d38a947e
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-01-24 14:21:17 +0100

    add javadoc comments in MapTypes.java

--- src/main/java/it/unibo/model/map/api/MapTypes.java
+++ src/main/java/it/unibo/model/map/api/MapTypes.java
@@ -4,29 +4,36 @@ package it.unibo.model.map.api;
  * enumeration of the various objects present in the map.
  */
 public enum MapTypes {
-    // 0 pickable 1 no-pickable 2 spawn-pac-man 3-spawn-ghost 4-gate-ghost 5 wall
+
+    // 0 pickable 1 no-pickable 2 spawn-pac-man 3-spawn-ghost 4-gate-ghost 5 wall.
+
     /**
-     * pickable objects
+     * pickable objects.
      */
     PICKABLE,
+
     /**
-     * no-pickable objects
+     * no-pickable objects.
      */
     NO_PICKABLE,
+
     /**
-     * spawn pac-man
+     * spawn pac-man.
      */
     SPAWN_PAC_MAN,
+
     /**
-     * spawn ghosts
+     * spawn ghosts.
      */
     SPAWN_GHOST,
+
     /**
-     * gate-ghosts
+     * gate-ghosts.
      */
     GATE_GHOST,
+
     /**
-     * walls
+     * walls.
      */
     WALL;
 }
