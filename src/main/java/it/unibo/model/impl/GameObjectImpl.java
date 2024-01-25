6dc0749cca0af29090d19140a10d421709e107a1
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-01-25 11:25:55 +0100

    Created GameObjectImpl.java file

---
+++ src/main/java/it/unibo/model/impl/GameObjectImpl.java
@@ -0,0 +1,39 @@
+package it.unibo.model.impl;
+
+import java.awt.Point;
+import java.awt.geom.Dimension2D;
+import java.net.URL;
+
+import it.unibo.model.api.GameObject;
+
+/** Basic Implementation of a GameObject. */
+public class GameObjectImpl implements GameObject {
+
+     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Point getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public URL getImageUrl() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getImageUrl'");
+    }
+
+     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Dimension2D getDimension() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDimension'");
+    }
+
+}
