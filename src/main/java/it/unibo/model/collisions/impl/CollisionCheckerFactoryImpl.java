32a8fa4c6f08cf0bb97fe385879706dd7ad57e01
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2024-01-26 10:51:33 +0100

    Added method gameObjectChecker in factory implementation

---
+++ src/main/java/it/unibo/model/collisions/impl/CollisionCheckerFactoryImpl.java
@@ -0,0 +1,15 @@
+package it.unibo.model.collisions.impl;
+
+import it.unibo.model.api.GameObject;
+import it.unibo.model.collisions.api.CollisionChecker;
+import it.unibo.model.collisions.api.CollisionCheckerFactory;
+
+public class CollisionCheckerFactoryImpl implements CollisionCheckerFactory{
+
+    @Override
+    public CollisionChecker<GameObject> gameObjectChecker() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameObjectChecker'");
+    }
+    
+}
