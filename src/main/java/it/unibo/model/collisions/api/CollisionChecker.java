c36411ed756f6f792611e7b73913201a7d8a0c5c
Nicolò Penserini <nicolo.penserini@studio.unibo.it>
2024-01-26 10:09:43 +0100

    Created interface CollisionChecker

---
+++ src/main/java/it/unibo/model/collisions/api/CollisionChecker.java
@@ -0,0 +1,18 @@
+package it.unibo.model.collisions.api;
+
+/**
+ * This interface models an object that can check if two objects of the same
+ * type are colliding.
+ * @param <T> the type of the two objects we want to check
+ */
+@FunctionalInterface
+public interface CollisionChecker<T> {
+
+    /**
+     * Method to check if two objects are colliding.
+     * @param firstObject the first of the two objects
+     * @param secondObject the second of the two objects
+     * @return true if the two objects are colliding, false otherwise
+     */
+    boolean areColliding(T firstObject, T secondObject);
+}
