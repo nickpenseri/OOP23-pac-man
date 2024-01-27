a1a6ec8a96a08c093ace75d1355092c54b94632f
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-01-27 19:06:20 +0100

    Created Pacman.java, the main class of the game

---
+++ src/main/java/it/unibo/Pacman.java
@@ -0,0 +1,20 @@
+package it.unibo;
+
+import it.unibo.core.api.Engine;
+import it.unibo.core.impl.EngineImpl;
+
+
+/** Main Class for start the game. */
+public final class Pacman {
+
+    private Pacman() { }
+
+    /**
+     * Main method for start the game.
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+        final Engine engine = new EngineImpl();
+        engine.mainLoop();
+    }
+}
