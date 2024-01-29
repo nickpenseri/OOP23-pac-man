4afe672050f6bc8c7138a77d712b8b87f37211c1
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-01-29 18:33:18 +0100

    created TestMapBuilder.java

---
+++ src/test/java/it/unibo/model/map/TestMapBuilder.java
@@ -0,0 +1,75 @@
+package it.unibo.model.map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.awt.Point;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.model.api.GameObject;
+import it.unibo.model.impl.GameObjectImpl;
+import it.unibo.model.map.impl.MapBuilderImpl;
+
+/**class that runs tests on MapBuilder. */
+class TestMapBuilder {
+
+    private static final int[][] SAMPLE_MAP = {
+            { 5, 5, 5, 5, 5 },
+            { 5, 0, 0, 0, 5 },
+            { 5, 0, 2, 0, 5 },
+            { 5, 3, 3, 3, 5 },
+            { 5, 5, 5, 5, 5 }
+    };
+
+    private MapBuilderImpl mapBuilder;
+
+    @BeforeEach
+    void setUp() {
+        mapBuilder = new MapBuilderImpl(SAMPLE_MAP);
+    }
+
+    @Test
+    void testGetSpawnGhost() {
+        List<Point> spawnGhost = mapBuilder.getSpawnGhost();
+        assertEquals(3, spawnGhost.size());
+        assertEquals(new Point(3, 1), spawnGhost.get(0));
+        assertEquals(new Point(3, 2), spawnGhost.get(1));
+        assertEquals(new Point(3, 3), spawnGhost.get(2));
+    }
+
+    @Test
+    void testGetPacManSpawn() {
+        Point pacManSpawn = mapBuilder.getPacManSpawn();
+        assertEquals(new Point(2, 2), pacManSpawn);
+    }
+
+    @Test
+    void testGetSpawnCollectibleItems() {
+        List<Point> spawnCollectibleItems = mapBuilder.getSpawnCollectibleItems();
+        assertEquals(5, spawnCollectibleItems.size());
+        assertTrue(spawnCollectibleItems.contains(new Point(1, 1)));
+        assertTrue(spawnCollectibleItems.contains(new Point(1, 2)));
+        assertTrue(spawnCollectibleItems.contains(new Point(1, 3)));
+        assertTrue(spawnCollectibleItems.contains(new Point(2, 3)));
+        assertTrue(spawnCollectibleItems.contains(new Point(2, 1)));
+    }
+
+    @Test
+    void testGetWallsPath() {
+        List<GameObject> wallsPath = mapBuilder.getWallsPath();
+        assertEquals(16, wallsPath.size());
+        assertTrue(wallsPath.get(0).getPosition().equals(new Point(0, 0)));
+        assertTrue(wallsPath.get(1).getPosition().equals(new Point(0, 1)));
+        assertTrue(wallsPath.get(2).getPosition().equals(new Point(0, 2)));
+    }
+
+    @Test
+    void testGetObjectsMap() {
+        GameObjectImpl[][] objectsMap = mapBuilder.getObjectsMap();
+        assertEquals(SAMPLE_MAP.length, objectsMap.length);
+        assertEquals(SAMPLE_MAP[0].length, objectsMap[0].length);
+    }
+}
