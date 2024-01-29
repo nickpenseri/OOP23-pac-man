761ab3e21c1bb76474d467501ef7a870e19fda38
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-01-29 14:55:28 +0100

    Test Created

--- src/test/java/it/unibo/model/physics/objectsmover/TestPositionApproximator.java
+++ src/test/java/it/unibo/model/physics/objectsmover/TestPositionApproximator.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.awt.Dimension;
 import java.awt.Point;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,19 +25,36 @@ import it.unibo.model.physics.objectsmover.impl.PositionApproximatorImpl;
  */
 class TestPositionApproximator {
    
-    //private PositionApproximator approximator;
+    private PositionApproximator approximator;
     private GameObjectFactory factory;
 
     /** Configuration method */
     @BeforeEach
     void test() {
-       //approximator = new PositionApproximatorImpl();
+       approximator = new PositionApproximatorImpl();
        factory = new GameObjectFactoryImpl();
     }
 
 
+      /**
+      * Test the behaviour of getApproximatedPosition, test if is always near to something.
+      */
     @Test
     void EmptyTest() {
-        assertTrue(new Dimension(10, 10).equals(factory.createGameObjectWithEmptyGraphics(new Point(0, 0), new Dimension(10, 10), GameObjectImpl.Type.PASSABLE).getDimension()));
+        var target = factory.createGameObjectWithEmptyGraphics(new Point(10, 10), new Dimension(10, 10), GameObjectImpl.Type.PASSABLE);
+        List<GameObject> list = IntStream.range(0, 10)
+        .mapToObj(i -> factory.createGameObjectWithEmptyGraphics(new Point(i * 10, i * 10), new Dimension(10, 10), GameObjectImpl.Type.PASSABLE))
+        .collect(Collectors.toList());
+        
+        assertFalse(list.get(0).equals(approximator.getApproximatedPosition(target, list).get()));
+        assertTrue(list.get(1).equals(approximator.getApproximatedPosition(target, list).get()));
+
+        target = factory.createGameObjectWithEmptyGraphics(new Point(30, 30), new Dimension(10, 10), GameObjectImpl.Type.PASSABLE);
+        assertFalse(list.get(4).equals(approximator.getApproximatedPosition(target, list).get()));
+        assertTrue(list.get(3).equals(approximator.getApproximatedPosition(target, list).get()));
+
+        target = factory.createGameObjectWithEmptyGraphics(new Point(200, 200), new Dimension(10, 10), GameObjectImpl.Type.PASSABLE);
+        assertFalse(approximator.getApproximatedPosition(target, list).isEmpty());
+
      }
 }
