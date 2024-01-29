fbb72f6656260cd7d8ee8ac18bbe1551b1eb6041
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-01-29 17:50:59 +0100

    fixed TestMapReader.java

--- src/test/java/it/unibo/model/map/TestMapReader.java
+++ src/test/java/it/unibo/model/map/TestMapReader.java
@@ -3,7 +3,6 @@ package it.unibo.model.map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.model.map.impl.MapReaderImpl;
@@ -11,17 +10,12 @@ import it.unibo.model.map.impl.MapReaderImpl;
  * this class is used to test the MapReader.java.
  */
 class TestMapReader {
-    private int[][] actualMap;
+    private final int[][] actualMap;
     private static final int W = 5;
-    private static final int G = 4;
-    private static final int P = 2;
     private static final int O = 0;
-    private static final int N = 1;
-    private static final int S = 3;
 
     /**method that creates a MapReader object for testing. */
-    @BeforeEach
-    void setUp() {
+    TestMapReader() {
         final String filePath = "src/main/resources/map1.txt";
         final MapReaderImpl mapReader = new MapReaderImpl(filePath);
         this.actualMap = mapReader.getMap();
@@ -30,44 +24,18 @@ class TestMapReader {
     /**method that checks whether the map has been read correctly. */
     @Test
     void testGetMap() {
-        assertNotNull(this.actualMap, "actualMap non è stata inizializzata");
+        assertNotNull(this.actualMap, "actualMap was not initialized");
         final int[][] expectedMap = {
                 { W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W },
                 { W, O, O, O, O, O, O, O, O, O, O, O, W, O, O, O, O, O, O, O, O, O, O, O, W },
-                { W, O, W, W, W, W, O, W, W, W, W, O, W, O, W, W, W, W, O, W, W, W, W, O, W },
-                { W, O, W, W, W, W, O, W, W, W, W, O, W, O, W, W, W, W, O, W, W, W, W, O, W },
-                { W, O, W, W, W, W, O, W, W, W, W, O, W, O, W, W, W, W, O, W, W, W, W, O, W },
-                { W, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, W },
-                { W, O, W, W, W, W, O, W, O, W, W, W, W, W, W, W, O, W, O, W, W, W, W, O, W },
-                { W, O, W, W, W, W, O, W, O, W, W, W, W, W, W, W, O, W, O, W, W, W, W, O, W },
-                { W, O, O, O, O, O, O, W, O, O, O, O, W, O, O, O, O, W, O, O, O, O, O, O, W },
-                { W, W, W, W, W, W, O, W, W, W, W, N, W, N, W, W, W, W, O, W, W, W, W, W, W },
-                { W, W, W, W, W, W, O, W, N, N, N, N, N, N, N, N, N, W, O, W, W, W, W, W, W },
-                { W, W, W, W, W, W, O, W, N, W, W, W, G, W, W, W, N, W, O, W, W, W, W, W, W },
-                { O, O, O, O, O, O, O, N, N, W, W, S, S, S, W, W, N, N, O, O, O, O, O, O, O },
-                { W, W, W, W, W, W, O, W, N, W, W, W, W, W, W, W, N, W, O, W, W, W, W, W, W },
-                { W, W, W, W, W, W, O, W, N, N, N, N, N, N, N, N, N, W, O, W, W, W, W, W, W },
-                { W, W, W, W, W, W, O, W, N, W, W, W, W, W, W, W, N, W, O, W, W, W, W, W, W },
-                { W, O, O, O, O, O, O, O, O, O, O, O, W, O, O, O, O, O, O, O, O, O, O, O, W },
-                { W, O, W, W, W, W, O, W, W, W, W, O, W, O, W, W, W, W, O, W, W, W, W, O, W },
-                { W, O, W, W, W, W, O, W, W, W, W, O, W, O, W, W, W, W, O, W, W, W, W, O, W },
-                { W, O, O, O, W, W, O, O, O, O, O, O, P, O, O, O, O, O, O, W, W, O, O, O, W },
-                { W, W, W, O, W, W, O, W, W, O, W, W, W, W, W, O, W, W, O, W, W, O, W, W, W },
-                { W, W, W, O, W, W, O, W, W, O, W, W, W, W, W, O, W, W, O, W, W, O, W, W, W },
-                { W, O, O, O, O, O, O, W, W, O, O, O, W, O, O, O, W, W, O, O, O, O, O, O, W },
-                { W, O, W, W, W, W, W, W, W, W, W, O, W, O, W, W, W, W, W, W, W, W, W, O, W },
-                { W, O, W, W, W, W, W, W, W, W, W, O, W, O, W, W, W, W, W, W, W, W, W, O, W },
-                { W, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O, W },
-                { W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W }
+                { W, O, W, W, W, W, O, W, W, W, W, O, W, O, W, W, W, W, O, W, W, W, W, O, W }
         };
-        //final int[][] actualMap = this.mapReader.getMap();
-        assertEquals(expectedMap.length, this.actualMap.length);
+        assertEquals(expectedMap[0].length, this.actualMap[0].length);
         for (int i = O; i < expectedMap.length; i++) {
             assertEquals(expectedMap[i].length, this.actualMap[i].length);
             for (int j = O; j < expectedMap[i].length; j++) {
                 assertEquals(expectedMap[i][j], this.actualMap[i][j]);
             }
         }
-
     }
 }
