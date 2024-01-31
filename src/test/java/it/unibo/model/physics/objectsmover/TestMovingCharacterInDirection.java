3d8651cd31503a6890092c2e52b6421780a48b5f
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-01-31 10:16:23 +0100

    Created test for change Position based on the DirectionSelector

---
+++ src/test/java/it/unibo/model/physics/objectsmover/TestMovingCharacterInDirection.java
@@ -0,0 +1,76 @@
+package it.unibo.model.physics.objectsmover;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+import it.unibo.model.api.Direction;
+import it.unibo.model.api.GameObject;
+import it.unibo.model.api.GameObjectFactory;
+import it.unibo.model.ghost.impl.GhostImpl;
+import it.unibo.model.impl.GameObjectFactoryImpl;
+import it.unibo.model.api.Character;
+import it.unibo.model.physics.objectsmover.api.DirectionSelector;
+import it.unibo.model.physics.objectsmover.impl.DirectionSelectorImpl;
+
+class TestMovingCharacterInDirection {
+    private static final int GAME_OBJ_SIZE = 10;
+    private static final int INIT_POSITION = 10;
+
+    private final Dimension dim = new Dimension(GAME_OBJ_SIZE, GAME_OBJ_SIZE);
+    private final DirectionSelector selector = new DirectionSelectorImpl();
+    private final GameObjectFactory factory  = new GameObjectFactoryImpl();
+
+    @Test
+    void moveUp() {
+        final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1);
+        final GameObject target = factory.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION, INIT_POSITION + 1), dim);
+        int yDiff = ghost.getPosition().y - target.getPosition().y;
+        selector.setDirection(ghost, target);
+        ghost.updateState(1000);
+        int yDiffAfter = ghost.getPosition().y - target.getPosition().y;
+        assertTrue(Math.abs(yDiffAfter) < Math.abs(yDiff));
+       
+    }
+
+
+    @Test
+    void moveDown() {
+        final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1);
+        final GameObject target = factory.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION, INIT_POSITION - 1), dim);
+        int yDiff = ghost.getPosition().y - target.getPosition().y;
+        selector.setDirection(ghost, target);
+        ghost.updateState(1000);
+        int yDiffAfter = ghost.getPosition().y - target.getPosition().y;
+        assertTrue(Math.abs(yDiffAfter) < Math.abs(yDiff));
+       
+    }
+
+
+    @Test
+    void moveRight() {
+        final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1);
+        final GameObject target = factory.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION + 1, INIT_POSITION), dim);
+        int xDiff = ghost.getPosition().x - target.getPosition().x;
+        selector.setDirection(ghost, target);
+        ghost.updateState(1000);
+        int xDiffAfter = ghost.getPosition().x - target.getPosition().x;
+        assertTrue(Math.abs(xDiffAfter) < Math.abs(xDiff));
+       
+    }
+
+    @Test
+    void moveLeft() {
+        final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1);
+        final GameObject target = factory.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION - 1, INIT_POSITION), dim);
+        int xDiff = ghost.getPosition().x - target.getPosition().x;
+        selector.setDirection(ghost, target);
+        ghost.updateState(1000);
+        int xDiffAfter = ghost.getPosition().x - target.getPosition().x;
+        assertTrue(Math.abs(xDiffAfter) < Math.abs(xDiff));
+       
+    }
+}
