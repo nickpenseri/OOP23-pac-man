ba7e99cb501e4c40d90957cd1566ab88d2afde6e
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-01-31 15:00:48 +0100

    Writed GraphDirectionSelector

--- src/main/java/it/unibo/model/physics/objectsmover/impl/GraphDirectionSelector.java
+++ src/main/java/it/unibo/model/physics/objectsmover/impl/GraphDirectionSelector.java
@@ -7,31 +7,62 @@ import it.unibo.model.physics.objectsmover.api.PositionApproximator;
 
 import org.jgrapht.alg.shortestpath.AStarShortestPath;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DefaultUndirectedGraph;
 
 import java.util.Objects;
 
 import org.jgrapht.Graph;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm.SingleSourcePaths;
-import org.jgrapht.alg.shortestpath.AStarShortestPath;
 
+/**
+ * Implementation of the interface DirectionSelector, sets the direction of a character to move to reach a target 
+ * moving on a graph.
+ */
 public class GraphDirectionSelector implements DirectionSelector {
 
-    final private Graph<GameObject, DefaultEdge> graph;
-    final private PositionApproximator approximator;
+    private final Graph<GameObject, DefaultEdge> graph;
+    private final PositionApproximator approximator;
+    private final DirectionSelector selectDir;
 
+    /**
+     * Creates a new GraphDirectionSelector.
+     * @param graph the graph of the gameMap
+     */
     public GraphDirectionSelector(final Graph<GameObject, DefaultEdge> graph) {
         this.graph = Objects.requireNonNull(graph);
         this.approximator = new PositionApproximatorImpl();
+        selectDir = new EuclideanDirectionSelector();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void setDirection(Character toMove, GameObject target) {
+    public void setDirection(final Character toMove, final GameObject target) {
         Objects.requireNonNull(toMove);
         Objects.requireNonNull(target);
 
-        var source = approximator.getApproximatedPosition(toMove, graph.vertexSet());
+        final var sourceVertex = approximator.getApproximatedPosition(toMove, graph.vertexSet());
+        if (!sourceVertex.isPresent()) {
+            throw new IllegalArgumentException("The source is not in the graph");
+        }
+
+        final var targetVertex = approximator.getApproximatedPosition(target, graph.vertexSet());
+        if (!targetVertex.isPresent()) {
+            throw new IllegalArgumentException("The target is not in the graph");
+        }
+
+        final AStarShortestPath<GameObject, DefaultEdge> aStarAlg = new AStarShortestPath<>(this.graph, (s, t) -> {
+            final double dx = s.getPosition().x - t.getPosition().x;
+            final double dy = s.getPosition().y - t.getPosition().y;
+            return Math.sqrt(dx * dx + dy * dy);
+        });
+
+        final SingleSourcePaths<GameObject, DefaultEdge> aPaths = aStarAlg.getPaths(sourceVertex.get());
+        final var path = aPaths.getPath(targetVertex.get());
+        if (path.getVertexList().size() >= 2) {
+            selectDir.setDirection(toMove, path.getVertexList().get(1));
+        } else {
+            toMove.resetDirection();
+        }
     }
-    
 }
