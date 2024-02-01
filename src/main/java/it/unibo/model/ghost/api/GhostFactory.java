3016fce77f28de17781b39d960947c7c2389946c
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-02-01 12:37:24 +0100

    Implemented GhostFactoryImpl

--- src/main/java/it/unibo/model/ghost/api/GhostFactory.java
+++ src/main/java/it/unibo/model/ghost/api/GhostFactory.java
@@ -1,9 +1,6 @@
 package it.unibo.model.ghost.api;
 
-import java.awt.Dimension;
 import java.awt.Point;
-
-
 /**
  * This interface models an entity of GhostFactory, an object that is responsible of
  * creating ghosts.
@@ -12,32 +9,32 @@ public interface GhostFactory {
     /**
      * Create a new red ghost.
      * @param pos the position of the ghost
-     * @param dimension the dimension of the ghost
+     * @param initialSpeed the initial speed of the ghost
      * @return the new ghost
      */
-    Ghost createRedGhost(Point pos, Dimension dimension);
+    Ghost createRedGhost(Point pos, double initialSpeed);
 
     /**
      * Create a new blue ghost.
      * @param pos the position of the ghost
-     * @param dimension the dimension of the ghost
+     * @param initialSpeed the initial speed of the ghost
      * @return the new ghost
      */
-    Ghost createBlueGhost(Point pos, Dimension dimension);
+    Ghost createBlueGhost(Point pos, double initialSpeed);
 
     /**
      * Create a new pink ghost.
      * @param pos the position of the ghost
-     * @param dimension the dimension of the ghost
+     * @param initialSpeed the initial speed of the ghost
      * @return the new ghost
      */
-    Ghost createPinkGhost(Point pos, Dimension dimension);
+    Ghost createPinkGhost(Point pos, double initialSpeed);
 
     /**
      * Create a new orange ghost.
      * @param pos the position of the ghost
-     * @param dimension the dimension of the ghost
+     * @param initialSpeed the initial speed of the ghost
      * @return the new ghost
      */
-    Ghost createOrangeGhost(Point pos, Dimension dimension);
+    Ghost createOrangeGhost(Point pos, double initialSpeed);
 }
---
+++ src/main/java/it/unibo/model/ghost/impl/GhostFactoryImpl.java
@@ -0,0 +1,61 @@
+package it.unibo.model.ghost.impl;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+import it.unibo.model.ghost.api.Ghost;
+import it.unibo.model.ghost.api.GhostColor;
+import it.unibo.model.ghost.api.GhostFactory;
+
+/**
+ * This class represents an implementation of {@link GhostFactory}.
+ */
+public class GhostFactoryImpl implements GhostFactory {
+
+    private final Dimension dimension;
+
+    /**
+     * Creates an object of this class.
+     * @param width the width of the ghost
+     * @param height the height of the ghost
+     */
+    public GhostFactoryImpl(final int width, final int height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Width and Height must be positive");
+        } 
+       dimension = new Dimension(width, height);
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Ghost createRedGhost(final Point pos, final double initialSpeed) {
+        return new GhostImpl(pos, dimension, initialSpeed, new GhostGraphics(GhostColor.RED));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Ghost createBlueGhost(final Point pos, final double initialSpeed) {
+        return new GhostImpl(pos, dimension, initialSpeed, new GhostGraphics(GhostColor.BLUE));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Ghost createPinkGhost(final Point pos, final double initialSpeed) {
+        return new GhostImpl(pos, dimension, initialSpeed, new GhostGraphics(GhostColor.PINK));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Ghost createOrangeGhost(final Point pos, final double initialSpeed) {
+        return new GhostImpl(pos, dimension, initialSpeed, new GhostGraphics(GhostColor.ORANGE));
+    }
+}
