15f384276668167e474f7baa71da85b9c27a9863
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2024-02-02 16:51:33 +0100

    Updated TestMovingCharacterInDirection

--- src/test/java/it/unibo/model/physics/objectsmover/TestMovingCharacterInDirection.java
+++ src/test/java/it/unibo/model/physics/objectsmover/TestMovingCharacterInDirection.java
@@ -13,6 +13,7 @@ import it.unibo.model.ghost.api.GhostColor;
 import it.unibo.model.ghost.impl.GhostGraphics;
 import it.unibo.model.ghost.impl.GhostImpl;
 import it.unibo.model.impl.GameObjectFactoryImpl;
+import it.unibo.model.impl.GameObjectImpl;
 import it.unibo.model.api.Character;
 import it.unibo.model.physics.objectsmover.api.DirectionSelector;
 import it.unibo.model.physics.objectsmover.impl.DirectionSelectorImpl;
@@ -24,12 +25,12 @@ class TestMovingCharacterInDirection {
     private final GhostGraphics graphics = new GhostGraphics(GhostColor.BLUE);
     private final Dimension dim = new Dimension(GAME_OBJ_SIZE, GAME_OBJ_SIZE);
     private final DirectionSelector selector = new DirectionSelectorImpl();
-    private final GameObjectFactory fact = new GameObjectFactoryImpl(GAME_OBJ_SIZE, INIT_POSITION, GAME_OBJ_SIZE, GAME_OBJ_SIZE);
+    private final GameObjectFactory factory = new GameObjectFactoryImpl(GAME_OBJ_SIZE, INIT_POSITION, GAME_OBJ_SIZE, GAME_OBJ_SIZE);
 
     @Test
     void moveUp() {
-        final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1, graphics);
-        final GameObject target = fact.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION, INIT_POSITION + 1), dim);
+        final Character ghost = factory.createGhost(new Point(INIT_POSITION, INIT_POSITION), 1, GhostColor.BLUE);
+        final GameObject target =  factory.createGameObject(new Point(INIT_POSITION, INIT_POSITION + 1), GameObjectImpl.Type.FLOR);
         final int yDiff = ghost.getPosition().y - target.getPosition().y;
         selector.setDirection(ghost, target);
         ghost.updateState(1000);
@@ -40,7 +41,7 @@ class TestMovingCharacterInDirection {
     @Test
     void moveDown() {
         final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1,  graphics);
-        final GameObject target = fact.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION, INIT_POSITION - 1), dim);
+        final GameObject target =  factory.createGameObject(new Point(INIT_POSITION, INIT_POSITION - 1), GameObjectImpl.Type.FLOR);
         final int yDiff = ghost.getPosition().y - target.getPosition().y;
         selector.setDirection(ghost, target);
         ghost.updateState(1000);
@@ -51,7 +52,7 @@ class TestMovingCharacterInDirection {
     @Test
     void moveRight() {
         final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1, graphics);
-        final GameObject target = fact.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION + 1, INIT_POSITION), dim);
+        final GameObject target =  factory.createGameObject(new Point(INIT_POSITION + 1, INIT_POSITION), GameObjectImpl.Type.FLOR);
         final int xDiff = ghost.getPosition().x - target.getPosition().x;
         selector.setDirection(ghost, target);
         ghost.updateState(1000);
@@ -62,7 +63,7 @@ class TestMovingCharacterInDirection {
     @Test
     void moveLeft() {
         final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1,  graphics);
-        final GameObject target = fact.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION - 1, INIT_POSITION), dim);
+        final GameObject target =  factory.createGameObject(new Point(INIT_POSITION - 1, INIT_POSITION), GameObjectImpl.Type.FLOR);
         final int xDiff = ghost.getPosition().x - target.getPosition().x;
         selector.setDirection(ghost, target);
         ghost.updateState(1000);
@@ -73,7 +74,7 @@ class TestMovingCharacterInDirection {
     @Test
     void noMove() {
         final Character ghost = new GhostImpl(new Point(INIT_POSITION, INIT_POSITION), dim, 1, graphics);
-        final GameObject target = fact.createGameObjectWithEmptyGraphics(new Point(INIT_POSITION, INIT_POSITION), dim);
+        final GameObject target =  factory.createGameObject(new Point(INIT_POSITION, INIT_POSITION), GameObjectImpl.Type.FLOR);
         selector.setDirection(ghost, target);
         ghost.updateState(1000);
         assertEquals(target.getPosition(), ghost.getPosition());
