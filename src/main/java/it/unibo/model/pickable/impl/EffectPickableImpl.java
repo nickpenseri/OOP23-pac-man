d4526f5d5897b5d22a9782d66b93ccf823fdde48
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-02-05 17:57:05 +0100

    Modified pickable png

--- src/main/java/it/unibo/model/pickable/impl/EffectPickableImpl.java
+++ src/main/java/it/unibo/model/pickable/impl/EffectPickableImpl.java
@@ -11,7 +11,7 @@ import it.unibo.model.pickable.api.EffectPickable;
  * Is a basic Pickable with the bonus effect and give you 50 points.
  */
 public abstract class EffectPickableImpl extends PickableImpl implements EffectPickable {
-    static final URL IMAGE_URL = ClassLoader.getSystemResource("image/pickable/PickableEffectColor.png");
+    static final URL IMAGE_URL = ClassLoader.getSystemResource("image/pickable/PickableEffect.png");
     static final int POINTS = 50;
 
     /**
