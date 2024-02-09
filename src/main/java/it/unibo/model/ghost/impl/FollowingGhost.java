50dd6d8ea6e26889a50b88ff5c75d23dad2cc0c1
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2024-02-09 11:44:07 +0100

    Basic implementation of FollowingGhost

---
+++ src/main/java/it/unibo/model/ghost/impl/FollowingGhost.java
@@ -0,0 +1,85 @@
+package it.unibo.model.ghost.impl;
+
+import java.awt.Point;
+import java.awt.geom.Dimension2D;
+import java.net.URL;
+import java.util.Optional;
+
+import it.unibo.model.api.Direction;
+import it.unibo.model.ghost.api.Ghost;
+import it.unibo.model.ghost.api.GhostState;
+
+public class FollowingGhost implements Ghost{
+
+    final private Ghost ghost;
+
+    public FollowingGhost(Ghost ghost) {
+        this.ghost = ghost;
+    }
+
+    @Override
+    public int getSpeedLevel() {
+        return ghost.getSpeedLevel();
+    }
+
+    @Override
+    public void setState(GhostState state) {
+      ghost.setState(state);
+    }   
+
+    @Override
+    public GhostState getState() {
+        return ghost.getState();
+    }
+
+    @Override
+    public URL getImageUrl() {
+        return ghost.getImageUrl();
+    }
+
+    @Override
+    public boolean increaseSpeed() {
+        return ghost.increaseSpeed();
+    }
+
+    @Override
+    public boolean decreaseSpeed() {
+        return ghost.decreaseSpeed();
+    }
+
+    @Override
+    public void setDirection(Direction direction) {
+        ghost.setDirection(direction);
+    }
+
+    @Override
+    public void setPosition(Point position) {
+        ghost.setPosition(position);
+    }
+
+    @Override
+    public Optional<Direction> getDirection() {
+        return ghost.getDirection();
+    }
+
+    @Override
+    public void resetDirection() {
+        ghost.resetDirection();
+    }
+
+    @Override
+    public void updateState(long elapsed) {
+        ghost.updateState(elapsed);
+    }
+
+    @Override
+    public Point getPosition() {
+        return ghost.getPosition();
+    }
+
+    @Override
+    public Dimension2D getDimension() {
+        return ghost.getDimension();
+    }
+    
+}
