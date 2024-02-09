71db18c538581b5c21b2281b2ccbabefbe8f52ff
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-02-09 13:49:52 +0100

    add sound in pickablegeneretorimpl.java

--- src/main/java/it/unibo/model/pickable/impl/PickableGeneratorImpl.java
+++ src/main/java/it/unibo/model/pickable/impl/PickableGeneratorImpl.java
@@ -13,6 +13,8 @@ import it.unibo.model.pickable.api.EffectChose;
 import it.unibo.model.pickable.api.EffectPickable;
 import it.unibo.model.pickable.api.Pickable;
 import it.unibo.model.pickable.api.PickableGenerator;
+import it.unibo.view.api.SoundsEffect;
+import it.unibo.view.impl.SoundsEffectImpl;
 
 /** Is a Map Generetor of Pickable. */
 public class PickableGeneratorImpl implements PickableGenerator {
@@ -20,6 +22,7 @@ public class PickableGeneratorImpl implements PickableGenerator {
     private static final int PERCENTAGE = 100;
     private static final int PERCENTAGE_NORMAL_PICKABLE = 90;
     private static final int NUMBER_OF_ALL_EFFECT = EffectChose.values().length;
+    private final SoundsEffect sounds = new SoundsEffectImpl("/sound/bonus.wav");
 
     /**
      * Generate a Map of Pickable at PERCENTAGE_NORMAL_PICKABLE% of probability of
@@ -88,16 +91,19 @@ public class PickableGeneratorImpl implements PickableGenerator {
      * @param point  is the Point where the Pickable is
      * @param pacman is the PacMan that take the Pickable.
      * @param ghosts is the List of Ghosts.
+     * @throws Exception
      */
     @Override
     public void takePickable(final Point point, final PacMan pacman, final List<Ghost> ghosts) {
         if (pickableMap.containsKey(point)) {
             if (pickableMap.get(point) instanceof BonusPoints) {
                 ((EffectPickable) pickableMap.get(point)).doEffect(pacman, ghosts);
+                this.sounds.playSound();
             } else {
                 pickableMap.get(point).addPointsPickable(pacman);
                 if (pickableMap.get(point) instanceof EffectPickable) {
                     ((EffectPickable) pickableMap.get(point)).doEffect(pacman, ghosts);
+                    this.sounds.playSound();
                 }
             }
             pickableMap.remove(point);
