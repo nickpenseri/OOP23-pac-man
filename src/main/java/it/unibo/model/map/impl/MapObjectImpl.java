887f86174d459b4987812bd755dbe5a15862aa78
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2024-02-14 16:57:35 +0100

    fices mapobjectimpl.java

--- src/main/java/it/unibo/model/map/impl/MapObjectImpl.java
+++ src/main/java/it/unibo/model/map/impl/MapObjectImpl.java
@@ -4,34 +4,42 @@ import java.awt.Point;
 
 import it.unibo.model.map.api.MapObject;
 import it.unibo.model.map.api.MapTypes;
-/**class that creates a map object with its type and coordinate. */
-public class MapObjectImpl implements MapObject{
+
+/** class that creates a map object with its type and coordinate. */
+public class MapObjectImpl implements MapObject {
     private final Point coordinate;
     private final MapTypes type;
+
     /**
      * constructor that initializes the coordinate and type of the object.
-     * @param coordinate coordinates of the object within the map.
-     * @param type type of map.
+     * 
+     * @param type        type of map.
+     * @param coordinateX coordinate in x.
+     * @param coordinateY coordinate in y.
      */
-    public MapObjectImpl(final Point coordinate, final MapTypes type) {
-        this.coordinate = coordinate;
+    public MapObjectImpl(final int coordinateX, final int coordinateY, final MapTypes type) {
+        this.coordinate = new Point(coordinateY, coordinateX);
         this.type = type;
     }
-    /** 
+
+    /**
      * get the coordinates of the object.
+     * 
      * @return the coordinate
-    */
+     */
     @Override
     public Point getCoordinate() {
-        return new Point((int)this.coordinate.getY(), (int)this.coordinate.getX());
+        return new Point((int) this.coordinate.getY(), (int) this.coordinate.getX());
     }
+
     /**
      * get the type of the map object.
+     * 
      * @return type of the object.
      */
     @Override
     public MapTypes getType() {
         return this.type;
     }
-    
+
 }
