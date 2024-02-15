1cf5e232fe9d696586457728031645ecb4021dbb
Antonio Zarrilli <antonio.zarrilli@studio.unibo.it>
2024-02-15 12:13:23 +0100

    new grafic to end game menu

--- src/main/java/it/unibo/view/impl/EndGameViewImpl.java
+++ src/main/java/it/unibo/view/impl/EndGameViewImpl.java
@@ -6,6 +6,13 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.JPanel;
+import javax.swing.Box;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 import it.unibo.input.api.MenuCommand;
 import it.unibo.view.api.MenuView;
@@ -14,6 +21,7 @@ import it.unibo.view.api.MenuView;
  * Swing Implementation of EndGame.
  */
 public class EndGameViewImpl extends ViewImpl implements MenuView {
+    private static final int FONT_SIZE = 50;
     private static final long serialVersionUID = 1L;
     private final List<MenuCommand> commands;
 
@@ -28,13 +36,37 @@ public class EndGameViewImpl extends ViewImpl implements MenuView {
 
         final JLabel titleLabel = new JLabel("Score: " + score);
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        this.add(titleLabel);
+        titleLabel.setForeground(Color.WHITE);  // Set the text color to white
+        titleLabel.setFont(new Font(titleLabel.getFont().getName(), Font.PLAIN, FONT_SIZE));  // Set the font size to 20
+
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
         final JButton restart = new JButton();
         restart.setText("Restart");
-        this.add(restart);
         final JButton quit = new JButton();
         quit.setText("Quit");
-        this.add(quit);
+        final JPanel buttonPanel = new JPanel();
+        buttonPanel.add(restart);
+        buttonPanel.add(Box.createHorizontalStrut(10));
+        buttonPanel.add(quit);
+
+        final JPanel horizzontalPanel = new JPanel();
+        final GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        horizzontalPanel.setLayout(new GridBagLayout());
+        horizzontalPanel.add(titleLabel);
+        horizzontalPanel.add(Box.createVerticalStrut(100), gbc);
+        horizzontalPanel.add(buttonPanel);
+
+        final JPanel gridPanel = new JPanel(new GridBagLayout());
+
+        gridPanel.add(horizzontalPanel);
+        this.setLayout(new BorderLayout());
+        this.add(gridPanel, BorderLayout.CENTER);
+        this.setBackground(Color.BLUE);
+        buttonPanel.setOpaque(false);
+        horizzontalPanel.setOpaque(false);
+        gridPanel.setOpaque(false);
 
         restart.addActionListener(e -> commands.add(MenuCommand.START));
         quit.addActionListener(e -> {
